b82cd2408b9d068588dc04ab6983c076d1298467
NavarreteRenato <rnavarrete-27@hotmail.com>
2022-10-04 11:25:00 -0500

    configuracion de serverjs

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import bcrypt from 'bcrypt'
+import stripe from 'stripe'
+import {initializeApp} from 'firebase/app'
+
+//configuracion de firebase
+const firebaseConfig = {
+    
+}
